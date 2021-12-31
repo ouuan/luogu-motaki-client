c460425565e9c2177a09bc7c029eb5482fed2103
Yufan You <ouuansteve@gmail.com>
2021-12-31 18:17:37 +0800

    fix: improve log format

--- src/log.ts
+++ src/log.ts
@@ -1,4 +1,4 @@
 export default function log(type: 'info' | 'warn' | 'error', msg: string, head?: string) {
   // eslint-disable-next-line no-console
-  console[type](`${new Date().toISOString()} ${(head || type).toUpperCase().padEnd(8)} ${msg}`);
+  console[type](`${new Date().toISOString()} ${(head || type).toUpperCase().padStart(8)} ${msg}`);
 }
