0a3c13590cd7d11dcc0c20b92507612963fc60fc
Yufan You <ouuansteve@gmail.com>
2021-12-31 18:25:59 +0800

    fix: required referer

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const PAINTBOARD_URL = 'https://www.luogu.com.cn/paintboard';
 export const WS_URL = 'wss://ws.luogu.com.cn/ws';
 export const CD = 30000;
 export const CONFIG_FILE = 'motaki-config.json';
-export const REQUIRED_REFERER = 'https://www.luogu.com.cn/paintBoard';
+export const REQUIRED_REFERER = 'https://www.luogu.com.cn/paintboard';
